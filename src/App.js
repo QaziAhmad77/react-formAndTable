46b415c936cf465d9afbc977a86630a86becb75c
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-16 10:17:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -94,3 +94,159 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+// import { useState } from "react";
+// import "./App.css";
+// import data from "./mock-data.json";
+// import Form from "./components/Form";
+// import Filter from "./components/Filter";
+
+// function App() {
+//   const [list, setList] = useState(data);
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [id, setId] = useState("");
+//   const [nameError, setNameError] = useState(false);
+//   const [emailError, setEmailError] = useState(false);
+//   const [passwordError, setPasswordError] = useState(false);
+//   const [search, setSearch] = useState("");
+//   const deleteUser = (id) => {
+//     const filteredUsers = list.filter((item) => item.id !== id);
+//     setList(filteredUsers);
+//   };
+
+//   let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+//   const addUser = () => {
+//     const newArr = [...list];
+//     // newArr.push({ id: Math.floor(Math.random() * 100), name: name, email: email, password: password });
+//     // setList(newArr);
+//     if (!nameError && !emailError && !passwordError) {
+//       newArr.push({ id: Math.floor(Math.random() * 100), name: name, email: email, password: password });
+//       setList(newArr);
+//     }
+//   };
+//   const updateUser = () => {
+//     if (!nameError && !emailError && !passwordError) {
+//       const newArr = [...list];
+//       const index = newArr.findIndex((item) => item.id === id);
+//       newArr[index] = { id, name, email, password };
+//       setList(newArr);
+//     }
+//   };
+//   return (
+//     <>
+//       <body>
+//       <div className="allInputs">
+//         <h1>User Form</h1>
+//         <input
+//           type="text"
+//           value={name}
+//           required
+//           placeholder="Enter your name"
+//           onChange={(e) => {
+//             if (name.length < 4) {
+//               setNameError(true);
+//             } else {
+//               setNameError(false);
+//             }
+//             setName(e.target.value);
+//           }}
+//         />
+//         {nameError ? <span>Name length should be greater than 4</span> : ""}
+//         <input
+//           type="email"
+//           value={email}
+//           required
+//           placeholder="Enter your email"
+//           onChange={(e) => {
+//             if (!email.match(emailRegex)) {
+//               setEmailError(true);
+//             } else {
+//               setEmailError(false);
+//             }
+//             setEmail(e.target.value);
+//             console.log(e.target.value);
+//           }}
+//         />
+//         {emailError ? <span>Invalid email</span> : ""}
+//         <input
+//           type="password"
+//           value={password}
+//           required
+//           placeholder="Enter your password"
+//           onChange={(e) => {
+//             if (password.length < 5) {
+//               setPasswordError(true);
+//             } else {
+//               setPasswordError(false);
+//             }
+//             setPassword(e.target.value);
+//           }}
+//         />
+//         {passwordError ? <span>Password length should be greater than 5</span> : ""}
+//         <div className="btn-2">
+//           <button onClick={addUser}>Add</button>
+//           <button onClick={updateUser}>Update</button>
+//         </div>
+//       </div>
+//         <div className="container">
+//         <input
+//           className="searchBtn"
+//           type="text"
+//           placeholder="Search here"
+//           onChange={(e) => {
+//             setSearch(e.target.value);
+//           }}
+//         />
+//       </div>
+//         <h1 className="hClass">User Table</h1>
+//         <table>
+//           <thead>
+//             <th>id</th>
+//             <th>name</th>
+//             <th>email</th>
+//             <th>password</th>
+//             <th>Edit / Delete</th>
+//           </thead>
+//           <tbody>
+//             {list
+//               .filter((contact) => {
+//                 return search.toLocaleLowerCase() === "" ? contact : contact.fullName.toLocaleLowerCase().includes(search);
+//               })
+//               .map((item) => {
+//                 return (
+//                   <tr key={item.id}>
+//                     <td>{item.id}</td>
+//                     <td>{item.name}</td>
+//                     <td>{item.email}</td>
+//                     <td>{item.password}</td>
+//                     <td>
+//                       <button
+//                         onClick={() => {
+//                           setName(item.name);
+//                           setEmail(item.email);
+//                           setPassword(item.password);
+//                           setId(item.id);
+//                         }}>
+//                         edit
+//                       </button>
+//                       <button onClick={() => deleteUser(item.id)}>delete</button>
+//                     </td>
+//                   </tr>
+//                 );
+//               })}
+//           </tbody>
+//         </table>
+//       </body>
+//     </>
+//   );
+// }
+
+// export default App;
+
