d177d03540c618c952a9dadf70f7903c5425d0ac
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-18 02:28:40 +0500

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Form = ({ addData, handleOnchange, errors, addUser, updateUser }) => {
+  const navigate = useNavigate();
+  const goToBack = () => {
+    navigate("/table");
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateUser();
+    navigate("/table");
+  };
+  const handleSub = (event) => {
+    event.preventDefault();
+    addUser();
+    navigate("/table");
+  };
   return (
     <>
+      <div className="parent">
+        <button className="userBtn" onClick={goToBack}>
+          go to table
+        </button>
+      </div>
       <h1>User Form</h1>
       <form action="" className="allInputs">
         <input type="text" value={addData.name} name="name" required placeholder="Enter your name" onChange={handleOnchange} />
@@ -12,8 +32,12 @@ const Form = ({ addData, handleOnchange, errors, addUser, updateUser }) => {
         <input type="password" value={addData.password} name="password" required placeholder="Enter your password" onChange={handleOnchange} />
         {errors.password ? <span>Password should be at least 5 characters</span> : ""}
         <div className="btn-2">
-          <button onClick={addUser}>Add</button>
-          <button onClick={updateUser}>Update</button>
+          <button type="submit" onClick={handleSub}>
+            Add
+          </button>
+          <button type="submit" onClick={handleSubmit}>
+            Update
+          </button>
         </div>
       </form>
     </>
